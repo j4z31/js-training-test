6217b449dc5b12f47d6801b9ec6d7449fc222337
j4z31 <tumael_777@hotmail.com>
2015-09-29 15:13:07 -0400

    Exercise #2: Jasmine-node to biography.js corrected

--- jasmine-node-exercise/biography.js
+++ jasmine-node-exercise/biography.js
@@ -1,13 +1,3 @@
-/*var GetBio = function(name, age){
-	if (name === undefined || age === undefined)
-		throw 'Need name and age to provide a biography';
-	
-	if (typeof(age) != 'number' || age < 0)
-		throw 'Invalid age.'
-
-	return 'Hi, my name is ' + name + ' i am ' + age + ' years old and i am a nice guy.';	
-};*/
-
 var Biography = function(){
 };
 
